876c8bfd920579803dee6696402de4bcf62e1cc9
Sebastián Giraldo <joabgiraldo@gmail.com>
2023-01-13 12:55:48 -0500

    Sidebar + Search + Top Charts

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#19232bae]">
         <Searchbar />
 
-        <div className="px-6 h-[calc(100vh-24px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
